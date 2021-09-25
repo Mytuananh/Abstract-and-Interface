1b442f6a235dec9d0345fea975bf7326a94c484b
Your Name <you@example.com>
2021-09-26 02:55:52 +0700

    add

---
+++ src/animal/Chicken.java
@@ -0,0 +1,15 @@
+package animal;
+
+import edible.Edible;
+
+public class Chicken extends Animal implements Edible {
+    @Override
+    public String makesound() {
+        return "Chicken: cluck - cluck!";
+    }
+    @Override
+    public String howToEat() {
+        return "could be fried";
+    }
+
+}
---
+++ src/animal/Tiger.java
@@ -0,0 +1,8 @@
+package animal;
+
+public class Tiger extends Animal {
+    @Override
+    public String makesound() {
+    return "Tiger: roarrrr!";
+    }
+}
